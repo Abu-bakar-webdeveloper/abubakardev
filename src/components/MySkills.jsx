e2986c95e5e941872888cf528c70939a2b4add53
abubkar <abubakaraw8@gmail.com>
2024-08-15 22:16:47 +0500

    commit

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
 import { Mongodb, Reactjs, Nodejs, Redux, Express, Nextjs, Nextjs2, CSS3, HTML5, Javascript, Python } from "../../public/images";
 
